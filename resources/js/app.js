4b7cdf8a090cd36b5b6547c08ead3a8eccbf4f40
Naume-k <kobnaome@gmail.com>
2020-05-28 04:35:36 +0200

    adding app.js content

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
+require('./bootstrap');
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+import vuetify from './vuetify'
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,8 +12,20 @@
 
 require('./bootstrap');
 
+Vue.use(VueRouter)
 window.Vue = require('vue');
 
+import App from './Components/App.vue'
+import About from './Components/Pages/About.vue'
+import Home from './Components/Pages/Home.vue'
+import Activites from './Components/Pages/Activities.vue'
+import Projects from './Components/Pages/Projects.vue'
+import Gallery from './Components/Pages/Gallery.vue'
+import Donate from './Components/Pages/Donate.vue'
+import Join from './Components/Pages/JoinUs.vue'
+import Blog from './Components/Pages/Blog.vue'
+import singleProject from './Components/Pages/Projects/singleProject.vue'
+import birdTrack from './Components/Pages/Projects/birdTrack.vue'
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -21,6 +39,21 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: '/', name: 'home', component: Home,},
+        {path: '/about', name: 'about', component: About,},
+        {path: '/activities', name: 'activity', component: Activites,},
+        {path: '/projects', name: 'projects', component: Projects,},
+        {path: '/gallery', name: 'gallery', component: Gallery,},
+        {path: '/donate', name: 'donate', component: Donate,},
+        {path: '/join-us', name: 'join', component: Join,},
+        {path: '/join-us', name: 'blog', component: Blog,},
+        {path: '/projects/:slug', name: 'singleProject', component: singleProject,},
+        {path: '/project/bird-tracking', name: 'birdTrack', component: birdTrack,},
+    ],
+});
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +62,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    components: { App },
+    router,
+    vuetify,
 });
