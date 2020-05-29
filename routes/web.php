1a406ddcc796a5ab20aaaf9cd9d45eab3fe8f75c
Moise <muhiremoise004@gmail.com>
2020-05-29 06:10:19 +0200

    My Changes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'SpaController@index')->where('any', '.*');
+//Route::get('/{any}', 'SpaController@index')->where('any', '.*');
 
 Route::get('/activities', 'PagesController@activities')->name('activities');
 Route::get('/projects', 'PagesController@projects')->name('projects');
@@ -24,6 +24,6 @@ Route::get('/donate', 'PagesController@donate')->name('donate');
 
 
 
-// Route::group(['prefix' => 'admin'], function () {
-//     Voyager::routes();
-// });
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
