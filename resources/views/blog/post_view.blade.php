14f2f0ee1b4c639d5b97b34c8de8fbd20412f1db
Naume-k <kobnaome@gmail.com>
2020-06-01 09:00:41 +0200

    adding some redesign

--- resources/views/blog/post_view.blade.php
+++ resources/views/blog/post_view.blade.php
@@ -1,10 +1,10 @@
 @extends('blog.layout')
 @section('content')
-<div class="bg xs:h-84 xl:py-5 ">
+<div class="bg xl:py-5 ">
 
-    <div class="w-80 py-2 px-3 mx-auto bg-white mt-5 mb-4">
+    <div class="xl:w-80 md:w-90 py-2 px-3 mx-auto bg-white mt-5 mb-4">
         <div class="">
-            <h1 class="xl:text-3xl">{{ $post->title }}</h1>
+            <h1 class="xl:text-3xl xs:text-2xl">{{ $post->title }}</h1>
         </div>
         <div class="">
             <p class="xl:text-lg">{{ $post->excerpt }}</p>
