7e87c72e138d8950c7bdf7c0fbf7f2ffdf4897c5
Moise <muhiremoise004@gmail.com>
2020-08-20 17:10:01 +0200

    Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,12 @@
 @extends('spa')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <style>
     .bg{
         background-image: url('/img/cropped_1973.jpg');
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size: cover;
-        height: 615px;
-        
+        height: 615px;  
     }
     .line{
         height: 150px;
@@ -74,10 +74,10 @@
 
             </div>
             <div>
-                <div class="pb-2">
+                <!--<div class="pb-2">
                     <h2 class="text-white text-2xl">Gallery</h2>
-                </div>
-                <div class="grid grid-4">
+                </div>-->
+                <!--<div class="grid grid-4">
                     <div class="mr-3" data-aos="fade-up" data-aos-duration="1500">
                         <img class="rounded shadow-md" src="/img/IMG_8805.jpg" height="190" width="150">
                     </div>
@@ -90,10 +90,31 @@
                     <div class="mr-3" data-aos="fade-up" data-aos-duration="1500">
                         <img class="rounded shadow-md" src="/img/IMG_1973.jpg" height="190" width="150">
                     </div>
-                </div>
+                </div>-->
             </div>            
         </div>
 </div>
+<!--<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="/img/slide1.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" style="height: 615px" src="/img/IMG_8827.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" style="height: 615px" src="/img/IMG_8827.jpg" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>-->
 <div class="">
     <Services />
 </div>
@@ -124,4 +145,7 @@
 <div class="">
     <Blog/>
 </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 @endsection
